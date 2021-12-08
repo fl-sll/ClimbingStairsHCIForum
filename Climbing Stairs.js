30687f17d8beb6626197512789705bf001678e34
Edward Alvin K <68998054+fl-sll@users.noreply.github.com>
2021-12-08 21:51:58 +0700

    Add files via upload

---
+++ Climbing Stairs.js
@@ -0,0 +1,10 @@
+const minCost = (cost) => {
+    for (let i = cost.length - 3; ~i; i--){
+        cost[i] += Math.min(cost[i+1], cost[i+2]);
+    }
+    console.log(Math.min(cost[0], cost[1]));
+}
+minCost([0,2,2,1]);
+minCost([0,2,3,2]);
+minCost([10,15,20]);
+minCost([0,0,0,0,0,0]);
